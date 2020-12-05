7690d80d85956b7a164bb4e45b1965778e2a19cc
Patrick <patrick.nascimento@altimus.com.br>
2020-12-05 17:45:09 -0300

    game detail

--- src/components/GameDetailComponent.js
+++ src/components/GameDetailComponent.js
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 export default class GameDetail extends Component {
 
   constructor(props) {
     super(props)   
+
+    let name = this.props.location.pathname
+    const gameName = " " + name.substr(name.lastIndexOf("/")+1, name.length-1);
+    this.state = {
+        gameName : gameName,
+        board : ''
+    }
   }
 
+
+  componentDidMount() {
+
+    axios.get('https://frodosbg-api-git-master.patrickbnu.vercel.app/api/board?name='+this.state.gameName)
+      .then(res => {
+        //alert(JSON.stringify(res.data))
+        this.setState({
+          board: res.data.board
+        });
+      })
+      .catch((error) => {
+
+        this.setState({
+          board: error.message
+        });
+        //alert(JSON.stringify(error))
+        console.log("ERRRO: " +error);
+      })
+  }
+
+
   render() {
+   // const location = useLocation();
     return (<div className="table-wrapper">
       <p> <br />
       Em breve mais detalhes sobre o jogo 
-        {this.props.name}
+        {this.state.gameName}
+        <br/><br/>
+      json:   {this.state.board}
+ 
       </p>
     </div>);
   }
