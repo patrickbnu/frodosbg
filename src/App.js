ba96df2a4df759ef8a465b72c80ff4e25754d1e9
Patrick Nascimento <patrick.bnu@gmail.com>
2021-10-11 13:33:03 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -22,52 +22,54 @@ import logo from './images/logo.png';
 
 function App() {
   const [date, setDate] = useState(null);
+  
+  const [expanded, setExpanded] = useState(false);
+
   useEffect(() => {
-    /**async function getDate() { 
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();*/
+   
   }, []);
+
+
   return (
       <Router>
      
         <div className="App">
           <header className="App-header">
-            <Navbar className="navbar"  fixed="top" expand="lg">
+            <Navbar className="navbar"  fixed="top" expand="lg"  expanded={expanded}> 
               <Container>
 
-                <Navbar.Brand>
-                  <Link to={"/"} className="nav-link">
-                    <img width='100%' height={50} alt="" src={logo} />
-                  </Link>
-                </Navbar.Brand> 
+                  <Navbar.Brand>
+                    <Link to={"/"} className="nav-link">
+                      <img width='100%' height={50} alt="" src={logo} />
+                    </Link>
+                  </Navbar.Brand> 
 
-              <Navbar.Toggle />
-              <Navbar.Collapse  className="justify-content-end ">
+                  <Navbar.Toggle  onClick={() => setExpanded(expanded ? false : "expanded")} />
 
-                  <Nav>
-                    <Nav>
-                      <Link to={"/list"} className="nav-link navbar-right">
-                        Lista Aluguel
-                      </Link>
-                    </Nav>
+                  <Navbar.Collapse  className="justify-content-end ">
 
-                    <Nav>
-                      <Link to={{ pathname: "https://loja.frodosbg.com.br/?utm_campaign=site_aluguel&utm_medium=referral&utm_source=SiteAluguel" }} target="_blank"  className="nav-link navbar-right">
                      
-                        Loja 
-                      </Link>
-                    </Nav>                 
+                      <Nav >
+                        <Link to={"/list"} className="nav-link navbar-right" onClick={() => setExpanded(false)} >
+                          Lista Aluguel
+                        </Link>
+                      </Nav>
 
-                    <Nav>
-                      <Link to={"/about"} className="nav-link navbar-right">
-                        Sobre
-                      </Link>
-                    </Nav>
-                  </Nav>
-                </Navbar.Collapse>
+                      <Nav>
+                        <Link to={{ pathname: "https://loja.frodosbg.com.br/?utm_campaign=site_aluguel&utm_medium=referral&utm_source=SiteAluguel" }} target="_blank"  
+                        className="nav-link navbar-right" onClick={() => setExpanded(false)}>
+                      
+                          Loja 
+                        </Link>
+                      </Nav>                 
+
+                      <Nav >
+                        <Link to={"/about"} className="nav-link navbar-right" onClick={() => setExpanded(false)}>
+                          Sobre
+                        </Link>
+                      </Nav>
+                   
+                  </Navbar.Collapse>
               </Container>
             </Navbar>
           </header>
@@ -80,73 +82,24 @@ function App() {
                 <div className="wrapper">
                   <CacheSwitch>
                     <CacheRoute when="always" exact path='/' component={BoardList} />
-                    <Route  path="/about" component={About} />
-                    <CacheRoute when="always" path="/list" component={BoardList} />
-                    <Route path="/game" component={GameDetail} /> 
-                    <Route path="/store" component={() => { 
-                        window.open('https://loja.frodosbg.com.br/?utm_campaign=site_aluguel&utm_medium=referral&utm_source=SiteAluguel', '_blank');
-                        return true;
-                    }}/>
-                  
-
-
-
+                        <Route  path="/about" component={About} />
+                        <CacheRoute when="always" path="/list" component={BoardList} />
+                        <Route path="/game" component={GameDetail} /> 
+                        <Route path="/store" component={() => { 
+                            window.open('https://loja.frodosbg.com.br/?utm_campaign=site_aluguel&utm_medium=referral&utm_source=SiteAluguel', '_blank');
+                            return true;
+                        }}/>   
                   </CacheSwitch>
                 </div>
               </Col>
             </Row>
           </Container>
+
         </div>
       </Router>)
       
 ;
 
-/** 
-  return (
-    <main>
-      <h3>Aluguel de Jogos de Tabuleiro</h3>
-
-      <p>
-        <a href="https://bit.ly/aluguel-bnu" target="_blank"        >
-          Para mais detalhes acesse aqui.
-        </a>
-        <br />
-        <h5> Lista de Jogos abaixo</h5>
-      </p>
-      <BoardList/>
-      
-    </main>
-  );*/
-
-  function XX() {
-    return (
-      <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="#home">App</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav className="mr-auto">
-              <Nav.Link href="#foo">foo</Nav.Link>
-              <Nav.Link href="#bar">bar</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/1">action 1</NavDropdown.Item>
-                <NavDropdown.Item href="#action/2">action 2</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3">action 3</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/4">action 4</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#baz">baz</Nav.Link>
-              <Nav.Link eventKey={2} href="#qux">
-                qux
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </>
-    );
-  }
 
 }
 
