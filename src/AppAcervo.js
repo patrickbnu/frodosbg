3e893e340332642dd9ef2b8d3a826fc2fa6099c6
Patrick Nascimento <patrick.bnu@gmail.com>
2024-10-27 17:07:28 -0300

    Update AppAcervo.js

--- src/AppAcervo.js
+++ src/AppAcervo.js
@@ -2,7 +2,15 @@ import React, { useEffect } from 'react';
 
 function AppAcervo() {
   useEffect(() => {
-    window.location.href = 'https://frodosbg.acervodejogos.com.br/'; 
+    const path = window.location.pathname;
+
+    if (path === '/assinatura') {
+      window.location.href = 'https://frodosbg.acervodejogos.com.br/plans/index';
+    } if (path === '/loja') {
+      window.location.href = 'https://loja.frodosbg.com.br/';
+    } else {
+      window.location.href = 'https://frodosbg.acervodejogos.com.br/';
+    }
   }, []);
 
   return null; // Não renderiza nada, pois redireciona imediatamente
