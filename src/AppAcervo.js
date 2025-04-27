dac86731a04d1e2f7c1d014a52436ffefe31d0cb
Patrick Nascimento <patrick.bnu@gmail.com>
2025-04-27 20:28:21 -0300

    ajustes path

--- src/AppAcervo.js
+++ src/AppAcervo.js
@@ -3,15 +3,26 @@ import React, { useEffect } from 'react';
 function AppAcervo() {
   useEffect(() => {
     const path = window.location.pathname;
+    const hostname = window.location.hostname;
 
-    if (path === '/assinatura') {
-      window.location.href = 'https://frodosbg.acervodejogos.com.br/plans/index';
-    } else if (path === '/loja') {
-      window.location.href = 'https://loja.frodosbg.com.br/';
-    } else if (path === '/links') {
-      window.location.href = 'https://links.frodosbg.com.br/';
-    } else {
-      window.location.href = 'https://frodosbg.acervodejogos.com.br';
+    // Se já estiver no links.frodosbg.com.br, não faz nada
+    if (hostname === 'links.frodosbg.com.br') {
+      return;
+    }
+
+    switch (path) {
+      case '/assinatura':
+        window.location.href = 'https://frodosbg.acervodejogos.com.br/plans/index';
+        break;
+      case '/loja':
+        window.location.href = 'https://loja.frodosbg.com.br/';
+        break;
+      case '/links':
+        window.location.href = 'https://links.frodosbg.com.br/';
+        break;
+      default:
+        window.location.href = 'https://frodosbg.acervodejogos.com.br';
+        break;
     }
   }, []);
 
